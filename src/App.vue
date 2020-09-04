88221282c62deca5665987570a04c70d7108af4b
Lukasz Mordasiewicz <lmord@softserveinc.com>
2020-09-04 21:19:48 +0200

    fonts and hello component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Hello
+      title="Vue Poke Info"
+      subtitle="Get pokemon data. Data based at Pokémon API. Pokémon and Pokémon character names are trademarks of Nintendo."
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Hello from "./components/Hello.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Hello
   }
 };
 </script>
+
+<style>
+@font-face {
+  font-family: "Roboto";
+  src: url("./fonts/Roboto-Light.ttf");
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Roboto";
+  src: url("./fonts/Roboto-Bold.ttf");
+  font-weight: bold;
+  font-style: bold;
+}
+
+* {
+  font-family: "Roboto";
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1 class="hello__title">{{ title }}</h1>
+    <p v-if="subtitle" class="hello__subtitle">{{ subtitle }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    title: String,
+    subtitle: String
+  }
+};
+</script>
+
+<style lang="scss">
+.hello {
+  margin: 50px auto;
+  width: 600px;
+  text-align: center;
+
+  &__title {
+    font-weight: bold;
+    margin: 0 0 0.5rem;
+  }
+
+  &__subtitle {
+    margin: 0;
+  }
+}
+</style>
